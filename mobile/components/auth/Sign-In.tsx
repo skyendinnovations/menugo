db857077f8593100c5d9ea1a15ef5cf6213ae5fb
Sabareesh <23it185@skcet.ac.in>
2026-01-11 21:15:51 +0530

    feat(auth): enhance SignUp and SignIn forms with password visibility toggle and improved styling

--- mobile/components/auth/Sign-In.tsx
+++ mobile/components/auth/Sign-In.tsx
@@ -1,4 +1,4 @@
-import { View, Text } from 'react-native'
+import { View, Text, TouchableOpacity } from 'react-native';
 import { useState } from 'react';
 import { Stack, Link, useRouter } from 'expo-router';
 import { Button } from '@/components/ui/Button';
@@ -6,8 +6,18 @@ import { Input } from '@/components/ui/Input';
 import { Label } from '@/components/ui/Label';
 import { Container } from '@/components/ui/Container';
 import { getSession, signIn } from '@/lib/auth-client';
-import React from 'react'
+import React from 'react';
 import { authAPI } from '@/lib/api';
+import { Ionicons } from '@expo/vector-icons';
+import {
+  Card,
+  CardHeader,
+  CardTitle,
+  CardDescription,
+  CardContent,
+  CardFooter,
+} from '@/components/ui/Card';
+import { Alert } from '@/components/ui/Alert';
 
 export function SignInForm() {
   const router = useRouter();
@@ -15,7 +25,7 @@ export function SignInForm() {
   const [password, setPassword] = useState('');
   const [error, setError] = useState<string | null>(null);
   const [loading, setLoading] = useState(false);
-
+  const [showPassword, setShowPassword] = useState(false);
 
   async function onSubmit() {
     setLoading(true);
@@ -26,7 +36,7 @@ export function SignInForm() {
 
       if (result.data?.user) {
         const session = await getSession();
-        if(session.data?.user.role === 'admin') {
+        if (session.data?.user.role === 'admin') {
           router.replace('/(admin)');
         } else {
           router.replace('/(user)');
@@ -44,24 +54,56 @@ export function SignInForm() {
     <View className="flex flex-1 bg-black">
       <Stack.Screen options={{ title: 'Sign In', headerShown: false }} />
       <Container>
-        <Text className="text-2xl font-semibold mb-20 text-white mt-20">Welcome back!</Text>
-        {error ? <Text className="text-red-600 mb-2">{error}</Text> : null}
-        <View className="gap-3">
-          <View>
-            <Label nativeID="email" required>Email Address</Label>
-            <Input id="email" value={email} onChangeText={setEmail} placeholder="Enter your email" autoCapitalize="none" keyboardType="email-address" />
+        <Text className="mb-20 mt-20 text-2xl font-semibold text-white">Welcome back!</Text>
+
+        {error ? <Alert variant="destructive" title="Error" description={error} className='mb-10' /> : null}
+        <Card className="border-2 px-3 pb-20 pt-10">
+          <View className="gap-3">
+            <View>
+              <Label nativeID="email" required>
+                Email Address
+              </Label>
+              <Input
+                id="email"
+                value={email}
+                onChangeText={setEmail}
+                placeholder="Enter your email"
+                autoCapitalize="none"
+                keyboardType="email-address"
+              />
+            </View>
+            <View>
+              <Label nativeID="password" required>
+                Password
+              </Label>
+              <Input
+                id="password"
+                value={password}
+                onChangeText={setPassword}
+                placeholder="••••••••"
+                secureTextEntry={!showPassword}
+              />
+              <TouchableOpacity
+                onPress={() => setShowPassword(!showPassword)}
+                style={{ position: 'absolute', right: 10, top: 35 }}>
+                <Ionicons name={showPassword ? 'eye-off' : 'eye'} size={24} color="gray" />
+              </TouchableOpacity>
+            </View>
+            <Button
+              title={loading ? 'Signing in…' : 'Sign In'}
+              onPress={onSubmit}
+              disabled={loading || !email || !password}
+              className="mt-8 bg-red-600 py-2 "
+            />
+            <Link href="/(auth)/sign-up" asChild>
+              <Button
+                title="Create an account"
+                className="rounded bg-red-600 px-10 py-2  text-sm"
+              />
+            </Link>
           </View>
-          <View>
-            <Label nativeID="password" required>Password</Label>
-            <Input id="password" value={password} onChangeText={setPassword} placeholder="••••••••" secureTextEntry />
-          </View>
-          <Button title={loading ? 'Signing in…' : 'Sign In'} onPress={onSubmit} disabled={loading || !email || !password} className='bg-red-600 py-2 mt-8 '/>
-          <Link href="/(auth)/sign-up" asChild>
-            <Button title="Create an account" className="bg-red-600 py-2 px-10 text-sm  rounded" />
-          </Link>
-        </View>
+        </Card>
       </Container>
     </View>
   );
 }
-
